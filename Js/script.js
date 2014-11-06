766400df750c97a5d8b12ec1b578fc9d2de2a725
kurtau <kurt.ulm@gmail.com>
2014-11-06 00:34:11 -0800

    working basic

--- Js/script.js
+++ Js/script.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-	var grid = "<div class='grid'></div>";
-	for (var i = 0; i < 64; i++) {
-		$("#container").append(grid);
+
+	// makes grid squares
+	var blocks = 32;
+	for (var i = 0; i < blocks; i++) {
+		var square = "<div class='square'></div>";
+		for (var j = 1; j < blocks; j++) {
+			$("#container").append(square);
+		};
+		$("#container").append(square);
 	};
 
+	// changes background color when hovering
+	$(".square").hover(function() {
+		$(this).css({"background-color": "#E8866C"});
+	});
+
+	//
+	$("#reset").click(function() {
+		$(".square").css({"background-color" : "#494546"})
+	});
 
 });
